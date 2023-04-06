eb547d9b5f62b3a041471e865ced3c9d562506c4
seunggabi <seunggabi@naver.com>
2023-04-07 03:56:14 +0900

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # project-upbit
-for pypi 
+```shell
+python3 -m venv .venv
+source .venv/bin/activate
+
+python3 -m pip install --upgrade pip setuptools wheel twine pipenv
+```
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='upbit',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=['requests'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
